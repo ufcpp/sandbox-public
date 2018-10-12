5f43d7af0f9aaeaa29f41ccd19d3c17dde0b972c
Nobuyuki Iwanaga <ufcpp@live.jp>
2018-10-12 12:26:02 +0900

    ValueTask in Unity 2018.2.11f1

---
+++ Unity/2018 10 12 ValueTask/ClassLibrary1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static ValueTask M1() => new ValueTask(Task.Delay(1));
+        public static async ValueTask M2() => await Task.Delay(1);
+        public static ValueTask<object> M3() => new ValueTask<object>(Task.Delay(1).ContinueWith(_ => default(object)));
+        public static async ValueTask<object> M4() { await Task.Delay(1); return null; }
+        public static Task M5() => M1().AsTask();
+        public static async Task M6() => await M2();
+    }
+}
---
+++ Unity/2018 10 12 ValueTask/UnityProject/Assets/Test.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField] Button _button1;
+    [SerializeField] Button _button2;
+    [SerializeField] Button _button3;
+    [SerializeField] Button _button4;
+    [SerializeField] Button _button5;
+    [SerializeField] Button _button6;
+    [SerializeField] Text _text;
+
+    void Start()
+    {
+        _button1.onClick.AddListener(OnButton1); 
+        _button2.onClick.AddListener(OnButton2); 
+        _button3.onClick.AddListener(OnButton3); 
+        _button4.onClick.AddListener(OnButton4); 
+        _button5.onClick.AddListener(OnButton5);
+        _button6.onClick.AddListener(OnButton6);
+    }
+
+    private async void OnButton1()
+    {
+        await ClassLibrary1.Class1.M1();
+        _text.text = "1";
+    }
+
+    private async void OnButton2()
+    {
+        await ClassLibrary1.Class1.M2();
+        _text.text = "2";
+    }
+
+    private async void OnButton3()
+    {
+        await ClassLibrary1.Class1.M3();
+        _text.text = "3";
+    }
+
+    private async void OnButton4()
+    {
+        await ClassLibrary1.Class1.M4();
+        _text.text = "4";
+    }
+
+    private async void OnButton5()
+    {
+        await ClassLibrary1.Class1.M5();
+        _text.text = "5";
+    }
+
+    private async void OnButton6()
+    {
+        await ClassLibrary1.Class1.M6();
+        _text.text = "6";
+    }
+}
